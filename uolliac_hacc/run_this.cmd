b648d2a4776c2a3fbfb0df5f151adec5ad1575cb
michelle1574 <70703680+michelle1574@users.noreply.github.com>
2021-08-26 23:31:44 +0100

    Hack Uolliac's PC
    Just have admin rights and run script run_this.cmd

---
+++ uolliac_hacc/run_this.cmd
@@ -0,0 +1,36 @@
+@echo off
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+cd /d %~dp0
+echo You will hack Uolliac if you will press any key.
+echo Disclamer: You may have some restrictions after hacking.
+echo Exit if you don't want them.
+echo.
+echo.
+pause
+echo x=msgbox("You got hacked by Uolliac. Enjoy", vbOKOnly+vbInformation, "Oh no, see message below!") > C:\Windows\haha_uolliac_hacked.vbs
+reg import trouillac.reg
+echo.
+echo Registry settings that are required is applied.
+timeout /t 16 /nobreak >nul
+echo Uolliac PC is hacked. Enjoy!
+pause
+exit
